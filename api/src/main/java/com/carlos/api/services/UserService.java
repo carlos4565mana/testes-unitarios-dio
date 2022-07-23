78823c1a2f3b6f3ca29315510395ea65f370fe90
carlos4565mana <carloscal61@gmail.com>
2022-07-23 20:55:22 -0300

    end point findAll

--- api/src/main/java/com/carlos/api/services/UserService.java
+++ api/src/main/java/com/carlos/api/services/UserService.java
@@ -1,8 +1,12 @@
 package com.carlos.api.services;
 
+import java.util.List;
+
 import com.carlos.api.domain.User;
 
 public interface UserService {
 
 	User findById(Integer id);
+
+	List<User> findAll();
 }
--- api/src/main/java/com/carlos/api/services/impl/UserServiceImpl.java
+++ api/src/main/java/com/carlos/api/services/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.carlos.api.services.impl;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,10 @@ public class UserServiceImpl implements UserService {
 	public User findById(Integer id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFoundException("Object not found..."));
+	}
+	
+	public List<User> findAll(){
+		return repository.findAll();
 	}
 
 }
