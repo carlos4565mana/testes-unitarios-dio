78d866fc2e003d2f1eb68a9c832dc82a8b68be94
carlos4565mana <carloscal61@gmail.com>
2022-07-23 16:46:30 -0300

    Created User class

--- api/src/main/java/com/carlos/api/ApiApplication.java
+++ api/src/main/java/com/carlos/api/ApiApplication.java
@@ -3,11 +3,15 @@ package com.carlos.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.carlos.api.domain.User;
+
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+		
 	}
+	
 
 }
