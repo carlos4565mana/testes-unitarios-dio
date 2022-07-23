33e64d6bc8cb1395df08ccfaf8876f7a9a487e61
carlos4565mana <carloscal61@gmail.com>
2022-07-23 20:35:51 -0300

    providing a model mapper in the form of a Bean

--- api/src/main/java/com/carlos/api/resource/UserResource.java
+++ api/src/main/java/com/carlos/api/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.carlos.api.resource;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,19 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carlos.api.domain.User;
+import com.carlos.api.domain.dto.UserDTO;
 import com.carlos.api.services.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
-	
+	@Autowired
+	private ModelMapper mapper;
 	@Autowired
 	private UserService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Integer id){
+	public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
 		
-		return ResponseEntity.ok().body(service.findById(id));
+		return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
 	}
 
 }
