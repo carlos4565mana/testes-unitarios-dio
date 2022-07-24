885ad78c6fe563474bb8a172287383673a4d34f7
carlos4565mana <carloscal61@gmail.com>
2022-07-24 09:08:23 -0300

    Test for UserService class fidAll method.

--- api/src/test/java/com/carlos/api/services/impl/UserServiceImplTest.java
+++ api/src/test/java/com/carlos/api/services/impl/UserServiceImplTest.java
@@ -15,6 +15,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 import static  org.hibernate.validator.internal.util.Contracts.assertNotNull;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -80,7 +81,18 @@ class UserServiceImplTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAnListOfUsers() {
+        when(repository.findAll()).thenReturn(List.of(user));
+        List<User> response = service.findAll();
+
+        assertNotNull(response);
+        assertEquals(1,response.size());
+        assertEquals(User.class, response.get(0).getClass());
+
+        assertEquals(ID, response.get(0).getId());
+        assertEquals(NAME, response.get(0).getName());
+        assertEquals(EMAIL, response.get(0).getEmail());
+        assertEquals(PASSWORD, response.get(0).getPassword());
     }
 
     @Test
